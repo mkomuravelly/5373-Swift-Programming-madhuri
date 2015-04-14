3492b997711ca58d9257414dc181f9ecce06b7f0
M. Komuravelly <mkomuravelly@bo120-08-46818.local>
2015-04-14 17:22:21 -0500

    changes added

--- MyLocations/MyLocations/CustomTabBarController.swift
+++ MyLocations/MyLocations/CustomTabBarController.swift
@@ -2,16 +2,15 @@
 //  CustomTabBarController.swift
 //  MyLocations
 //
-//  Created by M. Komuravelly on 4/9/15.
+//  Created by M. Komuravelly on 4/14/15.
 //  Copyright (c) 2015 mwsu. All rights reserved.
 //
 
 import Foundation
 import UIKit
-import MapKit
 
 // This class holds the data for my model.
-class PointData {
+class ModelData {
     var lat = 0.0
     var lon = 0.0
 }
@@ -20,7 +19,7 @@ class CustomTabBarController: UITabBarController {
     
     // Instantiate the one copy of the model data that will be accessed
     // by all of the tabs.
-    var currentPoint = PointData()
+    var currentPoint = ModelData()
     
     override func viewDidLoad() {
         super.viewDidLoad()
--- MyLocations/MyLocations/SecondViewController.swift
+++ MyLocations/MyLocations/SecondViewController.swift
@@ -15,10 +15,13 @@ class SecondViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let currentPoint = (self.tabBarController as CustomTabBarController).currentPoint
         // 1
         let location = CLLocationCoordinate2D(
-            latitude: 51.50007773,
-            longitude: -0.1246402
+            //latitude: 51.50007773,
+            //longitude: -0.1246402
+            latitude: currentPoint.lat,
+            longitude: currentPoint.lon
         )
         // 2
         let span = MKCoordinateSpanMake(0.05, 0.05)
@@ -49,7 +52,17 @@ class SecondViewController: UIViewController {
         // This tab will simply access the data and display it when the view
         // appears.
         //latLabel.text = currentdata.lat
-        //longLabel.text = "\(currentdata.long)"
+        //longLabel.text = "\(currentdata.long)" 
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        //3
+        let annotation = MKPointAnnotation()
+        annotation.setCoordinate(location)
+        annotation.title = "Big Ben"
+        annotation.subtitle = "London"
+        mapView.addAnnotation(annotation)
     }
 
 }
